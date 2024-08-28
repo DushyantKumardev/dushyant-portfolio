507798e48f030759f7a8034a03041f9671ee2a16
DushyantKumardev <dushyant2836@gmail.com>
2024-08-28 22:10:38 +0530

    2nd update

--- js/script.js
+++ js/script.js
@@ -8,9 +8,11 @@ window.addEventListener('load', function() {
 
 // 2. Fetch Preloader From Components
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('components/preloader.html')
+    fetch('../../components/preloader.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('preloader').innerHTML = data;
         });
 });
+
+
