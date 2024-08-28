bdc3e528382a84138d68f158cb376ca5ec457667
DushyantKumardev <dushyant2836@gmail.com>
2024-08-28 11:10:23 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// 1. Preloader 
+window.addEventListener('load', function() {
+    setTimeout(() => {
+    // Hide the preloader
+    document.getElementById('preloader').style.display = 'none';
+    }, 1000);
+});
+
+// 2. Fetch Preloader From Components
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('components/preloader.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('preloader').innerHTML = data;
+        });
+});
