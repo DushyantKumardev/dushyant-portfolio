bf8c20e4daf4f17b8aa1ad038ab7f47838a60414
DushyantKumardev <dushyant2836@gmail.com>
2024-08-29 13:17:35 +0530

    update sidebar desktop and mobile responsive

--- js/script.js
+++ js/script.js
@@ -1,18 +1,36 @@
-// 1. Preloader 
-window.addEventListener('load', function() {
-    setTimeout(() => {
+// 1. Preloader
+window.addEventListener("load", function () {
+  setTimeout(() => {
     // Hide the preloader
-    document.getElementById('preloader').style.display = 'none';
-    }, 1000);
-});
-
-// 2. Fetch Preloader From Components
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('../../components/preloader.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('preloader').innerHTML = data;
-        });
+    document.getElementById("preloader").style.display = "none";
+  }, 1000);
 });
 
 
+
+// 2. Hide And Show Desktop Navbar (Aside)
+document.addEventListener("DOMContentLoaded", () => {
+  const hideSidebarBtn = document.getElementById("hideSidebarBtn");
+  const showSidebarBtn = document.getElementById("showSidebarBtn");
+  const aside = document.getElementById("navDesktop");
+  const main = document.getElementById("main");
+
+  // Hide The Nav Desktop
+  hideSidebarBtn.addEventListener("click", () => {
+    aside.classList.add("hidden");
+    main.classList.add("shifted");
+    setTimeout(() => {
+      showSidebarBtn.classList.remove("d-hidden");
+    }, 600);
+  });
+
+
+  // Show The Nav Desktop
+  showSidebarBtn.addEventListener("click", () => {
+    aside.classList.remove("hidden");
+    main.classList.remove("shifted");
+    setTimeout(() => {
+      showSidebarBtn.classList.add("d-hidden");
+    }, 100);
+  });
+});
